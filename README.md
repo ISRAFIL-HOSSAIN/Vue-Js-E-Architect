fb60ff3594bac7cd0092a2e81bd3d66baa9e3b62
jony166091 <israfilhossain166091@gmail.com>
2022-06-20 01:55:58 +0600

    Vue-Js-E-Architect-App

---
+++ README.md
@@ -0,0 +1,26 @@
+```
+use this command for npm installation : 
+npm i
+
+```
+
+
+##  Run This Project 
+
+Compiles and hot-reloads for development
+```bash
+npm run serve
+```
+
+Compiles and minifies for production
+```bash
+npm run build
+```
+
+### 🛠️ Technologies
+
+The following tools were used in the construction of the project:
+
+- [Vue.js](https://vuejs.org/)
+- [Vuetify.js](https://vuetifyjs.com/)
+- [Firebase](https://firebase.google.com/)
---
+++ src/components/HomeSection.vue
@@ -0,0 +1,217 @@
+<template>
+  <section id="hero">
+    <v-parallax dark src="@/assets/img/bgHero.jpg" height="750">
+      <v-row align="center" justify="center">
+        <v-col cols="10">
+          <v-row align="center" justify="center">
+            <v-col cols="12" md="6" xl="8">
+              <h1 class="display-2 font-weight-bold mb-4">E-Architect</h1>
+              <h1 class="font-weight-light">
+                We are E-Architect<br />
+                We know your Dream<br/></h1>
+                <h2> Our Plan is always fulfill your Dream </h2>
+
+              <v-btn
+                rounded
+                outlined
+                large
+                dark
+                @click="$vuetify.goTo('#contact')"
+                class="mt-5"
+              >
+                Contact Us
+                <v-icon class="ml-2">mdi-arrow-down</v-icon>
+              </v-btn>
+              <!-- <div class="video d-flex align-center py-4">
+                <a @click.stop="dialog = true" class="playBut">
+                  <svg
+                    version="1.1"
+                    xmlns="http://www.w3.org/2000/svg"
+                    xmlns:xlink="http://www.w3.org/1999/xlink"
+                    xmlns:a="http://ns.adobe.com/AdobeSVGViewerExtensions/3.0/"
+                    x="0px"
+                    y="0px"
+                    width="60px"
+                    height="60px"
+                    viewBox="0 0 213.7 213.7"
+                    enable-background="new 0 0 213.7 213.7"
+                    xml:space="preserve"
+                  >
+                    <polygon
+                      class="triangle"
+                      id="XMLID_18_"
+                      fill="none"
+                      stroke-width="7"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-miterlimit="10"
+                      points="73.5,62.5 148.5,105.8 73.5,149.1 "
+                    />
+
+                    <circle
+                      class="circle"
+                      id="XMLID_17_"
+                      fill="none"
+                      stroke-width="7"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-miterlimit="10"
+                      cx="106.8"
+                      cy="106.8"
+                      r="103.3"
+                    />
+                  </svg>
+                </a>
+                <p class="subheading ml-2 mb-0">Assista o vídeo</p>
+              </div> -->
+            </v-col>
+            <v-col cols="12" md="6" xl="4" class="hidden-sm-and-down"> </v-col>
+          </v-row>
+        </v-col>
+      </v-row>
+      <div class="svg-border-waves text-white">
+        <v-img src="@/assets/img/borderWaves.svg" />
+      </div>
+    </v-parallax>
+
+    <!-- aboutus  -->
+    <v-container fluid id="aboutus" class="mt-2">
+      <v-row align="center" justify="center">
+        <v-col cols="10">
+          <v-row align="center" justify="space-around">
+            <v-col cols="12" class="text-center">
+              <h1 class="font-weight-light display-2">About Us</h1>
+              <h1 class="font-weight-light">
+                We always respect your dreams. We will do our best to build<br/> your dream home with your hard earned money. 
+              </h1>
+              
+              
+            </v-col>
+            <v-col
+              cols="12"
+              sm="4"
+              class="text-center"
+              v-for="(aboutus, i) in aboutuses"
+              :key="i"
+            >
+              <v-hover v-slot:default="{ hover }">
+                <v-card
+                  class="card"
+                  shaped
+                  :elevation="hover ? 10 : 4"
+                  :class="{ up: hover }"
+                >
+                  <v-img
+                    :src="aboutus.img"
+                    max-width="100px"
+                    class="d-block ml-auto mr-auto"
+                    :class="{ 'zoom-efect': hover }"
+                  ></v-img>
+                  <h1 class="font-weight-regular">{{ aboutus.title }}</h1>
+                  <h4 class="font-weight-regular subtitle-1">
+                    {{ aboutus.text }}
+                  </h4>
+                </v-card>
+              </v-hover>
+            </v-col>
+          </v-row>
+        </v-col>
+      </v-row>
+    </v-container>
+    <!-- <v-dialog v-model="dialog" max-width="640px">
+      <v-card>
+        <youtube
+          :video-id="videoId"
+          @ready="ready"
+          @playing="playing"
+        ></youtube>
+      </v-card>
+    </v-dialog>
+    <div class="svg-border-waves">
+      <img src="~@/assets/img/wave2.svg" />
+    </div> -->
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      // dialog: false,
+      // videoId: "i8IvvHJssWE",
+      aboutuses: [
+        {
+          img: require("@/assets/img/2.jpg"),
+          title: "Interior Design",
+          text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        },
+        {
+          img: require("@/assets/img/1.jpg"),
+          title: "Exterior Design",
+          text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        },
+        {
+          img: require("@/assets/img/3.jpg"),
+          title: "Space Design",
+          text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        },
+      ],
+    };
+  },
+
+};
+</script>
+
+<style>
+
+.svg-border-waves .v-image {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  height: 3rem;
+  width: 100%;
+  overflow: hidden;
+}
+
+#hero {
+  z-index: 0;
+}
+.svg-border-waves img {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  margin-bottom: -2px;
+  z-index: -1;
+}
+
+.card {
+  min-height: 300px;
+  padding: 10px;
+  transition: 0.5s ease-out;
+}
+
+.card .v-image {
+  margin-bottom: 15px;
+  transition: 0.75s;
+}
+
+.card h1 {
+  margin-bottom: 10px;
+}
+
+.zoom-efect {
+  transform: scale(1.1);
+}
+
+.up {
+  transform: translateY(-20px);
+  transition: 0.5s ease-out;
+}
+</style>
+
+<style>
+section {
+  position: relative;
+}
+</style>
